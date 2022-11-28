3a1573474e8ab75fcd40035241a9344011609e5f
Najaf Haider <najabhaider96@gmail.com>
2022-11-28 21:27:32 +0500

    Add LCD library and I2C device finder and LCD firmware.

---
+++ LCD_ESP8266/LCD_ESP8266.ino
@@ -0,0 +1,40 @@
+#include <LiquidCrystal_I2C.h>
+
+//int lcdAddr = 0x3F; // 63 in decimal
+int lcdAddr = 0x27; // 39 in decimal
+
+LiquidCrystal_I2C lcd(lcdAddr, 16, 2);
+void setup() {
+  Serial.begin(115200);
+  Serial.println("\n\nESP Start\n");
+
+  // initialize the lcd
+  lcd.init();
+  // Turn on the backlight.
+  lcd.backlight();
+  lcd.clear();
+  // Move the cursor characters to the right and
+  // zero characters down (line 1).
+  lcd.setCursor(0, 0);
+  // Print HELLO to the screen, starting at 5,0.
+  lcd.print("LCD With ESP8266");
+
+  // Move the cursor to the next line and print
+  delay(3000);
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Assalam-o-Alikum");
+  lcd.setCursor(2, 1);
+  lcd.print("Hello World");
+  Serial.println("\n\nSetup End\n");
+  delay(5000);
+}
+
+void loop() {
+  lcd.clear();
+  lcd.setCursor(3, 0);
+  lcd.print("Welcome To");
+  lcd.setCursor(2, 1);
+  lcd.print("JP  Learning");
+  delay(1000);
+}
