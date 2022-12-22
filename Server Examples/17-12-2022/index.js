3a0635ee1d965ed237d0c733906d77899b6701b5
Najaf Haider <najabhaider96@gmail.com>
2022-12-22 22:23:55 +0500

    Merge all code.

--- Server Examples/17-12-2022/index.js
+++ Server Examples/17-12-2022/index.js
@@ -1,15 +1,144 @@
 /*
 Welcome to JP Learning
 */
+const express = require('express');
 const mqtt = require('mqtt');
-const mongoose = require('mongoose');
 const shortId = require('shortid');
+const mongoose = require('mongoose');
 
-const Events = require('./eventsModel');
+const app = express();
+const port = 8080;
+app.use(express.json());
 
-const client = mqtt.connect('mqtt://broker.hivemq.com:1883');
+const server = require('http').createServer(app);
+const io = require('socket.io')(server, {
+    cors: {
+        origin: '*'
+    }
+});
 
-const topic = "JPLearning_SensorData";
+// Import Models
+const Users = require('./models/usersModel');
+const Events = require('./models/eventsModel');
+
+// HTTP POST Method
+app.post('/users', async (req, res) => {
+    console.log('POST Request');
+
+    // res.send('Got a POST request')
+
+    let user = req.body;
+    console.log('body:', user)
+
+    user = new Users(user);
+    console.log('user1:', user)
+
+    user = await user.save();
+    console.log('user:', user)
+
+    return res.status(201).json(user);
+})
+
+// HTTP GET Method
+app.get('/users', async (req, res) => {
+    console.log('GET Request');
+
+    // res.send('Assalam-o-Alaikum Hello World! Welcome to JP Learning :)')
+
+    let users = await Users.find({});
+
+    console.log('users:', users)
+
+    return res.status(200).json(users);
+})
+
+// HTTP GET by ID Method
+app.get('/users/:id', async (req, res) => {
+    console.log('GET Request');
+
+    // res.send('Assalam-o-Alaikum Hello World! Welcome to JP Learning :)')
+
+    console.log('id:', req.params.id)
+    // let user = await Users.findById(req.params.id);
+    let user = await Users.findOne({ _id: req.params.id });
+
+    console.log('user:', user)
+
+    return res.status(200).json(user);
+})
+
+// HTTP PUT Method
+app.put('/users/:id', async (req, res) => {
+    console.log('PUT Request');
+
+    // res.send('Got a PUT request at / device')
+
+    console.log('id:', req.params.id)
+    console.log('body:', req.body)
+
+    // let user = await Users.findByIdAndUpdate(req.params.id, req.body);
+    let user = await Users.findOneAndUpdate({ _id: req.params.id }, req.body);
+
+    user = await Users.findById(req.params.id);
+
+    console.log('user:', user);
+
+    return res.status(200).json(user);
+})
+
+// HTTP DELETE Method
+app.delete('/users/:id', async (req, res) => {
+    console.log('DELETE Request');
+
+    // res.send('Got a DELETE request at /device')
+
+    console.log('id:', req.params.id)
+
+    // let user = await Users.findByIdAndDelete(req.params.id);
+    // let user = await Users.findByIdAndRemove(req.params.id);
+
+    // let user = await Users.findOneAndDelete({ _id: req.params.id });
+    let user = await Users.findOneAndRemove({ _id: req.params.id });
+
+    console.log('user:', user);
+
+    return res.status(200).json(user);
+})
+
+// HTTP GET by DeviceId and Type Method
+app.get('/events/deviceId/:deviceId/type/:type', async (req, res) => {
+    console.log('GET Request for Events');
+
+    // res.send('Assalam-o-Alaikum Hello World! Welcome to JP Learning :)')
+
+    console.log('deviceId:', req.params.deviceId)
+    console.log('type:', req.params.type)
+    let events = await Events.find({ device_id: req.params.deviceId, type: req.params.type });
+
+    console.log('events:', events)
+
+    return res.status(200).json(events);
+})
+
+// WebSocket(Socket.IO)
+
+io.on('connection', client => {
+    console.log('Client Connected');
+    console.log(client.id);
+
+    client.on('JPLearning_SensorData', async (data) => {
+        console.log('[WS Received] Data:', data);
+    })
+});
+
+
+setInterval(() => {
+    sendData('time', new Date().toTimeString());
+}, 1000);
+
+sendData = async (topic, msg) => {
+    io.emit(topic, msg);
+}
 
 // MongoDB Connection Success
 mongoose.connection.on('connected', async () => {
@@ -21,10 +150,19 @@ mongoose.connection.on('error', async (err) => {
     console.log('Error connecting MongoDb', err);
 });
 
-client.on('connect', async () => {
-    // Connect MongoDb.
+server.listen(port, async () => {
+    // Connect MongoDb
     await mongoose.connect('mongodb+srv://jplearning:KC8CdRpHe3QwPIm5@cluster0.7xqgz4q.mongodb.net/Practice?retryWrites=true&w=majority');
 
+    console.log(`Example app listening on port ${port}`)
+})
+
+// MQTT(HiveMQ)
+
+const client = mqtt.connect('mqtt://broker.hivemq.com:1883');
+const topic = "JPLearning_SensorData";
+
+client.on('connect', async () => {
     console.log('MQTT Connected');
 
     client.subscribe(topic);
@@ -46,73 +184,4 @@ saveData = async (data) => {
     console.log('Saved data:', data);
 
     await sendData('JPLearning_SensorData', data);
-}
-
-// Websocket
-
-// const io = require('socket.io')({
-//     cors: {
-//         origin: '*'
-//     }
-// });
-
-// io.on('connection', client => {
-//     console.log('Client Connected');
-//     console.log(client.id);
-
-// });
-
-// io.listen(8080);
-
-
-
-
-// const server = require('http').createServer();
-// const io = require('socket.io')(server, {
-//     cors: {
-//         origin: '*'
-//     }
-// });
-
-// io.on('connection', client => {
-//     console.log('Client Connected');
-//     console.log(client.id);
-// });
-
-// server.listen(8080, async () => {
-//     console.log('Server running on port 8080');
-// });
-
-
-
-const app = require('express')();
-const server = require('http').createServer(app);
-const io = require('socket.io')(server, {
-    cors: {
-        origin: '*'
-    }
-});
-
-io.on('connection', client => {
-    console.log('Client Connected');
-    console.log(client.id);
-
-    client.on('JPLearning_SensorData', async (data) => {
-        console.log('[WS Received] Data:', data);
-    })
-});
-
-server.listen(8080, async () => {
-    console.log('Server running on port 8080');
-
-});
-
-
-
-setInterval(() => {
-    sendData('time', new Date().toTimeString());
-}, 1000);
-
-sendData = async (topic, msg) => {
-    io.emit(topic, msg);
 }
